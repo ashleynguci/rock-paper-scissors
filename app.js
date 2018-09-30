1d2ae5d1b6c03e9ef6836bb5b4eaa0bc68932d85
ashleynguci <anh.nguynngctruc@gmail.com>
2018-10-01 01:21:24 +0300

    Donee nhe

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("userscore");
 const compScore_span = document.getElementById("compscore");
 const scoreboard_div = document.getElementsByClassName("scoreboard");
-const result =  document.getElementsByClassName("result");
+const result =  document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 
+///Create function to conver user input into readable text
+function Convert(letter){
+    if(letter ==="r") return "Rock";
+    if(letter ==="s") return "Scissor";
+    else return "Paper";
+    }
+
+function win(u,c){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore = compScore;
+    result.innerHTML = Convert(u) + " beats " + Convert(c) +". BEN WINS =))";
+    document.getElementById("we").src="images/ben.png";
+    document.getElementById("we").style.visibility = "visible";
+}
+function lose(u,c){
+    compScore++;
+    compScore_span.innerHTML = compScore;
+    userScore = userScore;
+    result.innerHTML = Convert(c) + " beats " + Convert(u) +". ARRI WINS =))";document.getElementById("we").src="images/Arri.png";
+    document.getElementById("we").style.visibility = "visible";
+}
+function draw(u,c){
+    userScore = userScore;
+    compScore = compScore;
+    result.innerHTML = Convert(u) + " is equal to " + Convert(c) +". DRAW =))";
+    document.getElementById("we").src="images/Dat.png";
+    document.getElementById("we").style.visibility = "visible";
+}
 
 function getComputerChoice(){
 const choices = ['r','p','s'];
@@ -16,17 +45,29 @@ const value = Math.floor(Math.random()*3);
 return choices[value];
 }
 
-///Whichtype is parameter that we put inside, we call function play("r"), play("p") later
-function play(Whichtype){
+///Benchoice is parameter that we put inside, we call function play("r"), play("p") later
+function play(Benchoice){
     const computerchoice = getComputerChoice();
-    console.log("Uuser  " + Whichtype);
-    console.log("BUpet  " + computerchoice)
+    switch(Benchoice + computerchoice){
+        case "rs":
+        case "sp":
+        case "pr":
+        win(Benchoice,computerchoice);
+        break;
+        case "sr":
+        case "ps":
+        case "rp":
+        lose(Benchoice,computerchoice);
+        break;
+        default:
+        draw(Benchoice,computerchoice);
+        break;
+    }
 }
 
-///why
+///why function has no parameter, we need to create anomynous function to put our function inside in addEventListener
 function main(){
-    rock_div.addEventListener("click",function()(play("r");
-
+    rock_div.addEventListener("click",function(){play("r");
 })
 paper_div.addEventListener("click",function(){play("p");
 
